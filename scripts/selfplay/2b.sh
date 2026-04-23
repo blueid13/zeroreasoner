804b3262d08bc2bc70883c171f7ff3d8219e910b
seungmin cha <108518794+blueid13@users.noreply.github.com>
2026-04-24 03:10:43 +0900

    Update 2b.sh

--- scripts/selfplay/2b.sh
+++ scripts/selfplay/2b.sh
@@ -22,7 +22,7 @@ python -m absolute_zero_reasoner.main_azr_ppo \
     data.max_response_length=8096 \
     actor_rollout_ref.model.path=Qwen/Qwen3.5-2B-Base \
     actor_rollout_ref.actor.optim.lr=1e-6 \
-    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.model.use_remove_padding=False \
     actor_rollout_ref.actor.ppo_mini_batch_size=128 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=16 \
     actor_rollout_ref.actor.use_kl_loss=False \
